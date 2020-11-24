8d34fd824c79948825dd4851f19c18a00cd9269d
Ugochukwu Umerie <upchibuzz@gmail.com>
2020-11-24 19:43:44 +0100

    Break down homepage into smaller components, finish styling the menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import Hompage from './homepage.component';
+import Hompage from './pages/homepage/homepage.component';
 
 function App() {
   return (
