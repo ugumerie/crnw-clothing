14843cb5b5e083e4f9e8ef74f74f09cf58d238e4
Ugochukwu Umerie <upchibuzz@gmail.com>
2020-11-29 01:57:37 +0100

    Create shop page with corresponding components and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Hompage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Hompage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
